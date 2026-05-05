01330b87ac132325ac06475d6cccedae6233b0f2
Дмитрий <gg4239931@gmail.com>
2026-05-05 23:12:20 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,41 @@
-const { src, dest, series, parallel, watch } = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('gulp-autoprefixer').default; // ← ВАЖНО: .default
 const cleanCss = require('gulp-clean-css');
 const rename = require('gulp-rename');
-const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync').create();
 
 const paths = {
-  html: { src: 'src/*.html', dest: 'dist/' },
-  styles: { src: 'src/scss/*.scss', dest: 'dist/css/' }
+  html: 'src/*.html',
+  scss: 'src/scss/*.scss',
+  dist: 'dist'
 };
 
 function html() {
-  return src(paths.html.src)
-    .pipe(dest(paths.html.dest))
+  return src(paths.html)
+    .pipe(dest(paths.dist))
     .pipe(browserSync.stream());
 }
 
 function styles() {
-  return src(paths.styles.src)
-    .pipe(plumber())
+  return src(paths.scss)
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({ cascade: false }))
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions'], cascade: false }))
     .pipe(cleanCss({ level: { 1: { specialComments: 0 } } }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(paths.styles.dest))
+    .pipe(dest(paths.dist + '/css'))
     .pipe(browserSync.stream());
 }
 
 function serve() {
-  browserSync.init({ server: { baseDir: 'dist' } });
-  watch(paths.html.src, html);
-  watch(paths.styles.src, styles);
+  browserSync.init({
+    server: { baseDir: paths.dist },
+    open: true,
+    notify: false,
+    port: 3000
+  });
+  watch(paths.html, html);
+  watch(paths.scss, styles);
 }
 
-exports.html = html;
-exports.styles = styles;
-exports.serve = serve;
-exports.default = parallel(styles, html, serve);
+exports.default = series(styles, html, serve);
